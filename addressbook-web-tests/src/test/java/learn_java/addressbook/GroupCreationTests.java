20452b5b60ec6ed19b2af7d0c03911b52177c15a
kotishe777 <65562279+kotishe777@users.noreply.github.com>
2020-07-29 23:36:59 +0300

    Тест заработал!!!

--- addressbook-web-tests/src/test/java/learn_java/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/learn_java/addressbook/GroupCreationTests.java
@@ -1,14 +1,12 @@
-package learn_java.addressbook;
+package com.example.tests;
 
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.fail;
+import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
   private WebDriver driver;
@@ -26,17 +24,26 @@ public class GroupCreationTests {
   @Test
   public void testGroupCreationTests() throws Exception {
     driver.get("http://localhost/addressbook/group.php");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.xpath("//body")).click();
+    driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
+    driver.findElement(By.name("group_name")).sendKeys("text1");
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test2");
+    driver.findElement(By.name("group_header")).sendKeys("text2");
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test3");
+    driver.findElement(By.name("group_footer")).sendKeys("text3");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
   }
--- addressbook-web-tests/src/test/java/learn_java/addressbook/GroupCreationTests2.java
+++ addressbook-web-tests/src/test/java/learn_java/addressbook/GroupCreationTests2.java
@@ -14,6 +14,7 @@ public class GroupCreationTests2 {
 
   @BeforeMethod
   public void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver","D:\\Documents\\QA\\Learn_Automation\\Programs\\geckodriver\\geckodriver.exe");
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
@@ -21,12 +22,14 @@ public class GroupCreationTests2 {
   }
 
   private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("user")).sendKeys(password);
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
