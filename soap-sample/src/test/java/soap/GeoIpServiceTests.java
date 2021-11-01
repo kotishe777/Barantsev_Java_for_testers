05a2acdd3bdd4a36b9f048f6518732066e896d20
kcherkashyna <80709310+kcherkashyna@users.noreply.github.com>
2021-11-02 00:17:21 +0200

    Changes 01.11.2021

---
+++ soap-sample/src/test/java/soap/GeoIpServiceTests.java
@@ -0,0 +1,19 @@
+package soap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+//    @Test
+//    public void testMyIp(){
+//        GeoIP deoIP = new GeoIpService().getGeoIpServiceSoap12().getGeoIP("194.28.29.152");
+//        Assert.assertEquals(geoIP.getCountryCode, "RUS");
+//    }
+//
+//    @Test
+//    public void testInvalidIp(){
+//        GeoIP deoIP = new GeoIpService().getGeoIpServiceSoap12().getGeoIP("194.28.29.xxx");
+//        Assert.assertEquals(geoIP.getCountryCode, "RUS");
+//    }
+}
