8003b16764db8351e3141054d9bf5b861ce3adeb
kcherkashyna <80709310+kcherkashyna@users.noreply.github.com>
2021-11-11 19:42:09 +0200

    Changed 11.11.2021

--- addressbook-web-tests/src/test/java/learn_java/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/learn_java/addressbook/tests/TestBase.java
@@ -6,10 +6,8 @@ import learn_java.addressbook.model.Groups;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -18,6 +16,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+@Listeners(MyTestListener.class)
 public class TestBase {
 
   Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -26,8 +25,9 @@ public class TestBase {
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
-  public void setUp() throws Exception {
+  public void setUp(ITestContext context) throws Exception {
     app.init();
+    context.setAttribute("app", app);
   }
 
   @AfterSuite(alwaysRun = true)
