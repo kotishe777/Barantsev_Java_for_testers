9de6220d808d624206608df57b8925410f60c9a0
kcherkashyna <80709310+kcherkashyna@users.noreply.github.com>
2021-11-23 21:03:50 +0200

    23.11.2021 Firefox browser

--- selenium-sample/src/test/java/FirstTest.java
+++ selenium-sample/src/test/java/FirstTest.java
@@ -3,15 +3,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import sun.security.krb5.internal.crypto.Des;
 
 import java.io.UnsupportedEncodingException;
 
@@ -23,11 +19,11 @@ public class FirstTest {
 
     @Before
     public void start() {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
-        caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-        driver = new InternetExplorerDriver(caps);
-        System.out.println(((HasCapabilities) driver).getCapabilities());
+
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+//        driver = new ChromeDriver(caps);
+//        System.out.println(((HasCapabilities) driver).getCapabilities());
 
 //        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("start-fullscreen");
@@ -38,9 +34,14 @@ public class FirstTest {
 //        driver = new ChromeDriver(caps);
 
 //        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
-//        driver = new ChromeDriver(caps);
+//        caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+//        caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+//        driver = new InternetExplorerDriver(caps);
 //        System.out.println(((HasCapabilities) driver).getCapabilities());
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        FirefoxProfile profile = new FirefoxProfile();
+        driver = new FirefoxDriver(caps);
 
         wait = new WebDriverWait(driver, 10);
     }
